6cd9b810a7857d025a7854b16c8af29f73d14494
Mohammed Altujjar <mohammed_.1996@live.com>
2023-09-28 18:21:51 +0400

    Events page finalized

--- productionWebsite/src/main/java/com/milanopalace/website/productionWebsite/ProductionWebsiteApplication.java
+++ productionWebsite/src/main/java/com/milanopalace/website/productionWebsite/ProductionWebsiteApplication.java
@@ -8,7 +8,7 @@ public class ProductionWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductionWebsiteApplication.class, args);
-	}
-
-}
-                                           
+	} 
+ 
+} 
+                                                                                                     
