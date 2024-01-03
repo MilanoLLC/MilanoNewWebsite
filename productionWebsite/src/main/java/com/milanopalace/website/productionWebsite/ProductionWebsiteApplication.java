6781c8bd1d7b14c3f3ad7ed131d599b088fb8199
Mohammed Altujjar <mohammed_.1996@live.com>
2024-01-03 14:29:35 +0400

    WA fixed

--- productionWebsite/src/main/java/com/milanopalace/website/productionWebsite/ProductionWebsiteApplication.java
+++ productionWebsite/src/main/java/com/milanopalace/website/productionWebsite/ProductionWebsiteApplication.java
@@ -10,4 +10,4 @@ public class ProductionWebsiteApplication {
 		SpringApplication.run(ProductionWebsiteApplication.class, args);
 	}                 
                                         
-}                                                   
+}                                                    
